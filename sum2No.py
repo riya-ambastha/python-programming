90bc2c6721e6495e48619ae3dc97edf247e9250f
riya-ambastha <ambasthariya58@gmail.com>
2024-06-28 10:47:25 +0530

    Add files via upload

---
+++ sum2No.py
@@ -0,0 +1,12 @@
+# name = input("enter name :")
+# age = int(input("enter age :"))
+# marks = float(input("enter marks :"))
+
+# print("welcome",name)
+# print("age=",age)
+# print("marks=",marks)
+
+num1 = int(input("enter first number :"))
+num2 = int(input("enter second number :"))
+
+sum = print("sum of two number is :",(num1 + num2))
