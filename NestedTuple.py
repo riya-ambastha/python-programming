d0cc77a3b840ce40c08054f598cd261a21936cd6
riya-ambastha <ambasthariya58@gmail.com>
2024-07-04 09:26:14 +0530

    Add files via upload

---
+++ NestedTuple.py
@@ -0,0 +1,3 @@
+nested_tuple = (("apple", "banana"), ("cherry", "date"))
+print(nested_tuple[0])      # Accessing the first nested tuple
+print(nested_tuple[1][1])   # Accessing "date" in the second nested tuple
