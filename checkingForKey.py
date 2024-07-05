531932a34530127238d9fc63e9dc51d9fafd9146
Riya Ambastha <ambasthariya58@gmail.com>
2024-07-05 13:42:13 +0530

    checkingForKey.py

---
+++ checkingForKey.py
@@ -0,0 +1,10 @@
+person = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+if "name" in person:
+    print("Name is in the dictionary")
+if "email" not in person:
+    print("Email is not in the dictionary")
+
